1eb709a7ee1db9904f236d37d4ae568845996753
maxawolff <maxawolff@hotmail.com>
2017-10-20 17:20:20 -0700

    added testing file and gitignore

--- mail_room.py
+++ mail_room.py
@@ -13,26 +13,34 @@ donation_history = {}
 
 def main():
     """Function runs once program is called, calls functions based on input."""
-    reply = input("""Would you like to send a thank you or create a report?
+    reply = input("""
+Would you like to send a thank you or create a report?
 Enter 1 to send a thank you
 Enter 2 to create a report
-Enter 3 to quit this script""")
+Enter 3 to quit this script
+
+""")
     if reply == '1':
         thank_you()
     elif reply == '2':
         create_report()
     elif reply == '3':
-        sys.exit
+        sys.exit()
     else:
-        print('Bad input! See console for acceptable responses')
+        print('''
+Bad input! See console for acceptable responses
+''')
         main()
 
 
 def thank_you():
     """Once send thank you is selected, calls functions based on input."""
-    name = input("""Enter the full name of the donator
+    name = input("""
+Enter the full name of the donator
 or enter list to see a list of all donators
-or type quit to return to the orignal prompt""")
+or type quit to return to the orignal prompt
+
+""")
     if type(name) == str:
         if name == 'list':
             print_names()
@@ -42,7 +50,9 @@ or type quit to return to the orignal prompt""")
             check_name(name)
             enter_donation_amount(name)
     else:
-        print('please enter a name')
+        print('''
+Please enter a name
+            ''')
         thank_you()
 
 
@@ -50,6 +60,11 @@ def print_names():
     """."""
     for name in list_of_donors:
         print(name)
+    if not list_of_donors:
+        print("""
+There are no donors in the sytem yet
+
+""")
     thank_you()
 
 
@@ -63,14 +78,19 @@ def check_name(name):
 
 def enter_donation_amount(name):
     """Prompt the user for how much was donated, calls approprate function."""
-    donation_amount = input("""Enter the amount of the donation
-or type quit to return to the orignal prompt""")
+    donation_amount = input("""
+Enter the amount of the donation or type quit to return to the orignal prompt
+
+""")
     if donation_amount == 'quit':
         main()
     try:
         donation_amount = float(donation_amount)
     except ValueError:
-        print('Please enter a valid donation amount')
+        print('''
+Please enter a valid donation amount
+
+''')
         enter_donation_amount(name)
 
     add_donation_history(donation_amount, name)
@@ -78,8 +98,9 @@ or type quit to return to the orignal prompt""")
 
 def add_donation_history(amount, name):
     """."""
-    print("""Thank you, {},
-for your generous donation of ${}""".format(name, amount))
+    print("""
+Thank you, {}, for your generous donation of ${}
+""".format(name, amount))
     donation_history[name].append(amount)
     main()
 
@@ -100,8 +121,10 @@ def create_report():
     sorted_donations = sorted(donation_information,
                               key=operator.itemgetter(1), reverse=True)
     for person in sorted_donations:
-        print('''Name of donator: {}
+        print('''
+Name of donator: {}
 Total amount donated: {}
 Number of donations: {}
-Average donation: {}'''.format(person[0], person[1], person[2], person[3]))
+Average donation: {}
+'''.format(person[0], person[1], person[2], person[3]))
     main()
