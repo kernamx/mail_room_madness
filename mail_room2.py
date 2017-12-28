7b17ecf434b7044754c239f7dd75f51ab81e6220
maxawolff <maxawolff@hotmail.com>
2017-12-28 12:28:43 -0800

    added and tested list donors function

--- mail_room2.py
+++ mail_room2.py
@@ -11,3 +11,31 @@ def add_donor(name):
     if name not in list_of_donors:
         list_of_donors.append(name)
         donation_history[name] = []
+
+
+def verify_donation(amount):
+    """Verify that a donation, entered as a string, can be converted to a float."""
+    try:
+        float(amount)
+        return True
+    except ValueError:
+        return False
+
+
+def list_donors():
+    """Return the names of all of the donors."""
+    if list_of_donors:
+        string_start = "The current donors are "
+        if len(list_of_donors) == 1:
+            return 'The only donor is ' + list_of_donors[0]
+        elif len(list_of_donors) == 2:
+            return 'The current donors are ' + list_of_donors[0] + ' and ' + list_of_donors[1]
+        elif len(list_of_donors) > 2:
+            for idx, donor in enumerate(list_of_donors):
+                if idx < len(list_of_donors - 1):
+                    string_start += (donor + ', ')
+                else:
+                    string_start += ('and ' + donor)
+            return string_start
+    else:
+        return "There are currently no donors"
--- test_mail_room.py
+++ test_mail_room.py
@@ -4,6 +4,11 @@ import pytest
 import pdb
 
 
+def test_list_donors_no_donors():
+    """Should return string stating that there are no donors."""
+    assert mail_room2.list_donors() == "There are currently no donors"
+
+
 def test_add_donor():
     """Test adding a donor."""
     mail_room2.add_donor('John')
@@ -22,3 +27,19 @@ def test_donation_history_updates_with_new_user():
     """If a new user is added, their donation history should be an empty dict."""
     assert mail_room2.donation_history["Steve"] == []
     assert mail_room2.donation_history["John"] == []
+
+
+def test_verify_amount_correct_string():
+    """Return True on convertable string."""
+    assert mail_room2.verify_donation('3.6') is True
+
+
+def test_verify_amount_incorrect_string():
+    """Return False on non-convertable string."""
+    assert mail_room2.verify_donation('three point six') is False
+
+
+def test_list_donors_returns_names_of_donors():
+    """The names of all donors should be in the reutrn statement."""
+    assert "Steve" in mail_room2.list_donors()
+    assert "John" in mail_room2.list_donors()
