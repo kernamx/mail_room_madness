59693fb40972a5cbb3d647a48cd7a4d7472ef511
maxawolff <maxawolff@hotmail.com>
2017-12-28 12:04:34 -0800

    tested for donation history

--- test_mail_room.py
+++ test_mail_room.py
@@ -1,6 +1,7 @@
 """Test file for the mail room module."""
 import mail_room2
 import pytest
+import pdb
 
 
 def test_add_donor():
@@ -10,8 +11,14 @@ def test_add_donor():
 
 
 def test_add_donor_already_in_list():
-    """Test adding a donor."""
-    mail_room2.list_of_donors.append('Steve')
+    """Test adding a donor already in list doesn't re add them."""
+    mail_room2.add_donor('Steve')
     assert len(mail_room2.list_of_donors) == 2
     mail_room2.add_donor("Steve")
     assert len(mail_room2.list_of_donors) == 2
+
+
+def test_donation_history_updates_with_new_user():
+    """If a new user is added, their donation history should be an empty dict."""
+    assert mail_room2.donation_history["Steve"] == []
+    assert mail_room2.donation_history["John"] == []
