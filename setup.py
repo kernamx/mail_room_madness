a42a749e7bdf891b59b21dfd415bd9118f60ef0b
maxawolff <maxawolff@hotmail.com>
2017-12-30 16:25:34 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="mail-room",
+    description="A python simulation of a mail room for a charity",
+    version=0.1,
+    author='Max Wolff',
+    author_email="maxawolff@hotmail.com",
+    license='MIT',
+    py_modules=['mail_room2'],
+    extras_require={'test': ['pytest', 'pytest-watch']},
+    entry_points={
+        'console_scripts': [
+            "mailroom = mail_room2:mail_room"
+        ]
+    }
+)
